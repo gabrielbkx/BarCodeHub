7c0fdbde1137831fbd2e1ed0d9941b97e85dd47b
Gabriel <oliveirafrerreira97@hotmail.com>
2025-02-27 23:20:13 -0300

    criação do projeto

---
+++ src/main/java/com/gabrielbkx/BarCodeHub/BarCodeHubApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielbkx.BarCodeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarCodeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarCodeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielbkx/BarCodeHub/BarCodeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbkx.BarCodeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarCodeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
